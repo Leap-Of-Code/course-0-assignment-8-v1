a6f5c9b24175fc3b41c8ad7ca952444ecd9f56fa
nicoleyip1876 <17805096+nicoleyip1876@users.noreply.github.com>
2020-04-23 00:22:05 +0000

    Updating all questions

--- fix-code-0/main.cpp
+++ fix-code-0/main.cpp
@@ -2,21 +2,35 @@
 #include <string>
 using namespace std;
 
+void SpaceCalculation(int row) {
+  int counter = 0;
+  while (counter < row) {
+    cout << ' ';
+    counter++;
+  }
+}
+
 void Triangle(int input) {
-  //int counter = input;
+  int row = 0;
   int starting = 0;
 
-  while (input >= 0) {
-    while (input > starting) {
-      cout << '#';
-      starting++;
+  if (input % 2 == 1 || input % 2 == 0) {
+    // First while loop is # of rows
+    while (input >= 0) {
+      while (input > starting) {
+        cout << '#';
+        starting++;
+      }
+      input = input - 2;
+      cout << endl;
+      row++;
+      SpaceCalculation(row);
+      starting = 0;
     }
-    input--;
-    cout << endl;
-    starting = 0;
-  }
-  
+  } 
 }
+
+
 
 int main() {
  int input;
--- invent-code-3/main.cpp
+++ invent-code-3/main.cpp
@@ -2,26 +2,88 @@
 #include <string>
 using namespace std;
 
-int GetInteger() {
-  int input;
-  cout << "Enter a integer: ";
-  cin >> input;
-  return input;
+/* Asks user for number input
+ * Returns:
+ *   integer: user_input_1, the number the user inputs
+ */
+int UserInput() {
+  std::cout << "Enter a number: ";
+  int user_input_1;
+  std::cin >> user_input_1;
+  return user_input_1;  
 }
 
-char GetMathFunction() {
-  char function;
-  bool function_is_valid = false;
-  while (!function_is_valid) {
-    cout << "Enter a math function. It must be one of the following: (+, -, *, /, %, =): "
-    cin >> function;
-    if (fuction == '+' || fuction == '-' || fuction == '*' || fuction == '/') {
-      function_is_valid = true;
-    }
+/* Asks user for operator
+ * Returns:
+ *   char: user_input_operator, the operator that the user enters
+ */
+char OperatorsInput() {
+  std::cout << "Enter operator (+ - * / %):" << std:: endl;
+  char user_input_operator;
+  std::cin >> user_input_operator;
+  return user_input_operator;
+}
+
+/* Depending on the operator the user entered, it outputs the solution to the equation
+ * Args:
+ *   char input_operator: operator that user input
+ *   int user_inputted: user input from main
+ *   int user_inputted_2: new user input
+ * Returns:
+ *   integer: answer, output of the equation
+ */
+int AnswerOfOperatorEntered(char input_operator, int user_inputted, int user_inputted_2) {
+  //takes the operator, user input from main function, and user input from call_equation to come up with the total
+  int answer = 0;
+  if (input_operator == '+') {
+    answer = user_inputted + user_inputted_2;
+  } else if (input_operator == '-') {
+    answer = user_inputted - user_inputted_2; 
+  } else if (input_operator == '*') {
+    answer = user_inputted * user_inputted_2; 
+  } else if (input_operator == '/') {
+    answer = user_inputted / user_inputted_2; 
+  } else if (input_operator == '%') {
+    answer = user_inputted % user_inputted_2; 
+  } 
+  return answer;
+}
+
+/* Asks user for operator, input, and executes the equation. These are repetitive steps so we call this function 4 times.
+ * Args:
+ *   user_input_from_main: user input from main 
+ *   user_input_operator: operator that user input
+ *   new_user_input: new user input
+ * Returns:
+ *   integer: answer, output of the equation
+ */
+int CallingOtherFunctions(int user_input_from_main, char user_input_operator){
+  int answer = 0;
+  if (user_input_operator == '=') {
+    return user_input_from_main;
+  } 
+  else {
+    int new_user_input =  UserInput();
+    // I don't know how to bring the [VALUE OF] user_input_1 from the main function.
+    // You can bring values from previous function to functions using arguements.
+    
+    //this takes the user input from of operator, user_input from the main function, and a new user_input. 
+    answer = AnswerOfOperatorEntered(user_input_operator, user_input_from_main, new_user_input);
+    //std :: cout << answer << std :: endl;
   }
-  return function;
+  return answer;
 }
 
 int main() {
-  return 0;
+  std::cout << "This code is calculator. Enter = for value to be returned. " << std::endl;
+  //Goes to CallingOtherFunctions in a while loop 
+  int answer = UserInput(); 
+  char user_input_operator = ' ';
+  cout << answer << endl;
+  while (!(user_input_operator == '=')) {
+    user_input_operator = OperatorsInput();
+    answer = CallingOtherFunctions(answer, user_input_operator);
+  }
+  std::cout << answer << endl;
+  return 0; 
 }
