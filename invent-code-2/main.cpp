b6e2f26de27f5c24f44d7f245a00ea41d7e38e7e
vivianpppp <63162644+vivianpppp@users.noreply.github.com>
2020-05-04 03:31:11 +0000

    Saving progress for all questions.

--- invent-code-2/main.cpp
+++ invent-code-2/main.cpp
@@ -41,6 +41,50 @@ void Pyramid(int user_input) {
   }
 }
 
+
+void NumericalPyramid(int user_input) {
+  int print_space = 0;
+  int originl_value = user_input / 2;
+  bool is_odd = (user_input % 2 != 0);
+
+  while (user_input > 0) {
+    int first_value = user_input / 2;
+    int print_hash = first_value;
+    int print_space1 = print_space;
+    while (print_hash > 0) {
+      while (print_space1 > 0) {
+        cout << " ";
+        print_space1--;
+      }
+      cout << print_hash;
+      print_hash--;
+      
+      if (print_hash == 0) {
+        int print_hash1 = print_hash;
+        while (print_hash1 >= 0 && print_hash1 <= first_value) {
+          cout << print_hash1;
+          print_hash1++;
+        }
+      }
+    }
+    cout << endl;
+    user_input = user_input - 2;
+    print_space++;
+    
+    if (user_input == 1 && is_odd) {
+      print_space = originl_value;
+      while (print_space > 0) {
+        cout << " ";
+        print_space--;
+      }
+      cout << "0";
+    }
+
+  }
+}
+
+
+
 int main() {
   int user_input;
   user_input = UserInput();
