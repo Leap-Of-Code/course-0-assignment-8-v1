3ee5a75a0c93402f756367e58ea1d04c5e6b744f
cyndisong28 <30833889+cyndisong28@users.noreply.github.com>
2020-05-04 23:17:45 +0000

    Saving progress for all questions.

--- invent-code-2/main.cpp
+++ invent-code-2/main.cpp
@@ -5,12 +5,33 @@ using namespace std;
 int PrintTriangle (int input) {;
   int value = input / 2;
   int number_pound = 1;
+  
 
-  while (number_pound < input) {
+  while (number_pound < input && value >= 0) {
+    cout << value;
+    if (input % 2 != 0 && value == 0) {
+      break;
+    }
+    else if (value == 1 ) {
+    break;
+    }
     value = value - 1;
     number_pound++;
   }
-  cout << value << endl;
+
+  while ( number_pound <= input) {
+    if (value == input / 2) {
+    break;
+    }
+    if (input % 2 == 0 && value == 1) {
+      cout << "1";
+      number_pound++;
+    }
+  value = value + 1;
+  cout << value;
+  number_pound++;
+  }
+
   string length_of_space;
   string text = to_string(value);
   int length_of_line1 = text.length();
