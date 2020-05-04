526ece5fcb10b65c08b399b4594067c351a36a49
vivianpppp <63162644+vivianpppp@users.noreply.github.com>
2020-05-04 02:09:30 +0000

    Saving progress for all questions.

--- invent-code-2/main.cpp
+++ invent-code-2/main.cpp
@@ -23,24 +23,21 @@ void Triangle(int user_input) {
 }
 
 void Pyramid(int user_input) {
-  bool is_even = (user_input % 2 == 0);
-
-  if (is_even) {
-    while (user_input) {
-      int print_hash = user_input;
-      while (print_hash > 0) {
-        int print_space = 0;
-        int row = 1;
-        while (print_space > 0 && print_space < row) {
-          cout >> " ";
-          print_space++;
-        }
-        cout << "#";
+  int print_space = 0;
+  while (user_input > 0) {
+    int print_hash = user_input;
+    int print_space1 = print_space;
+    while (print_hash > 0) {
+      while (print_space1 > 0) {
+        cout << " ";
+        print_space1--;
       }
-
+      cout << "#";
+      print_hash--;
     }
-  }
-
+    cout << endl;
+    user_input = user_input - 2;
+    print_space++;
   }
 }
 
@@ -54,6 +51,9 @@ int main() {
   cout << "Print out a pyramid." << endl;
   Pyramid(user_input);
 
+  user_input = UserInput();
+  cout << "Print out special numerical block pyramid." << endl;
+  NumericalPyramid(user_input);
 
   return 0;
 }
