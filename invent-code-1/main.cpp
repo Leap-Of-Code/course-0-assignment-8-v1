d48e0f5a9572b38dd8850fef0cf813e2287f7eb1
anaygupta2004 <55030203+anaygupta2004@users.noreply.github.com>
2020-11-15 05:23:11 +0000

    Saving progress for all questions.

--- invent-code-1/main.cpp
+++ invent-code-1/main.cpp
@@ -2,6 +2,28 @@
 #include <string>
 using namespace std;
 
+int GetUserInput() {
+  int number; 
+  cout << "Number for Factorial: ";
+  cin >> number; 
+  return number;
+}
+int Factorial(int number) {
+  int temp = number;
+
+  for (int i = 0; i <= number - 1; i++) {
+    temp *= number - 1;
+    number = number - 1; 
+  }
+  
+  return temp;
+}
+
 int main() {
+  int number = GetUserInput();
+  int factorial = Factorial(number);
+
+  cout << "The factorial of " << number << " is " << factorial << endl;
+
   return 0;
 }
