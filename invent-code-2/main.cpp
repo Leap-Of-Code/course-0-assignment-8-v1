7dc200686e6c64dcd38141a0796b63467d255711
anaygupta2004 <55030203+anaygupta2004@users.noreply.github.com>
2020-11-15 05:37:36 +0000

    Updating all questions

--- invent-code-2/main.cpp
+++ invent-code-2/main.cpp
@@ -2,6 +2,24 @@
 #include <string>
 using namespace std;
 
+void TriangleMaker () {
+  int input;
+  int x;
+  cout << "Input for Side: ";
+  cin >> input;
+  for (int i = input; i >= 0; i--) {
+    x = 0;
+    int a = i;
+    while (0 <= a) {
+      cout << "#";
+      a--;
+    }
+    cout << endl;
+  }
+
+}
+
 int main() {
+  TriangleMaker();
   return 0;
 }
